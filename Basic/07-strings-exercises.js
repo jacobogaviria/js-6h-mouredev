e86240e5860f92c5065f8b1904ea51d8374c1906
jacobogaviria <jacobogav15@gmail.com>
2026-01-02 23:59:49 -0500

    Añadir ejercicios de condicionales y mejorar la lógica de control de flujo

--- Basic/07-strings-exercises.js
+++ Basic/07-strings-exercises.js
@@ -5,20 +5,48 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=7226
 
 // 1. Concatena dos cadenas de texto
 
+    console.log('hola' + ' ' + 'mundo')
+
 // 2. Muestra la longitud de una cadena de texto
+
+    console.log('hola'.length)
 
 // 3. Muestra el primer y último carácter de un string
 
+    console.log('hola'[0])
+    console.log('hola'[3])
+
 // 4. Convierte a mayúsculas y minúsculas un string
+
+    console.log('hola'.toUpperCase())
+    console.log('HOLA'.toLowerCase())
 
 // 5. Crea una cadena de texto en varias líneas
 
-// 6. Interpola el valor de una variable en un string
+    console.log(`Esta es la línea 1
+                Esta es la línea 2
+                Esta es la línea 3`)
+
+// 6. Interpola el valor de una variable en un string}
+
+        const MyName = 'Jacobo'
+        console.log(`Hola, mi nombre es ${MyName}`)
 
 // 7. Reemplaza todos los espacios en blanco de un string por guiones
-
+        const texto = 'Curso de JavaScript Básico'
+        console.log(texto.replace(/ /g, '-'))
+        console.log(texto.split('a').join('-'))
 // 8. Comprueba si una cadena de texto contiene una palabra concreta
+
+        const frase = 'El perro de San Roque no tiene rabo'
+        console.log(frase.includes('perro'))
 
 // 9. Comprueba si dos strings son iguales
 
+        const string1 = 'Hola'
+        const string2 = 'hola'
+        console.log(string1 === string2)
+
 // 10. Comprueba si dos strings tienen la misma longitud
+
+        console.log(string1.length == string2.length)
--- Basic/09-conditionals-exercises.js
+++ Basic/09-conditionals-exercises.js
@@ -3,27 +3,119 @@ Clase 24 - Ejercicios: Condicionales
 Vídeo: https://youtu.be/1glVfFxj8a4?t=8652
 */
 
+let a = 10
+let b = 5
+let usuario = "admin"
+let password = "1234"
+let x = null
+let edad = 16
+let mes = "marzo"
+let idioma = "español"
 // if/else/else if/ternaria
 
+if (a + b === 15) {
+    console.log("La suma es 15")
+} else if (a + b > 15) {
+    console.log("La suma es mayor que 15")
+} else {
+    console.log("La suma es menor que 15")
+}
 // 1. Imprime por consola tu nombre si una variable toma su valor
+if (usuario === "admin") {
+    console.log("admin")
+}
 
 // 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
 
+if (usuario === "admin" && password == "1234") {
+    console.log("Acceso concedido")
+}
+
 // 3. Verifica si un número es positivo, negativo o cero e imprime un mensaje
 
+if (x === 0) {
+    console.log("El número es cero")
+} else if (x > 0) {
+    console.log("El número es positivo")
+} else {
+    console.log("El número es negativo")
+}
+
 // 4. Verifica si una persona puede votar o no (mayor o igual a 18) e indica cuántos años le faltan
+if (edad >= 18) {
+    console.log("Puedes votar")
+} else {
+    console.log(`Te faltan ${18 - edad} años para poder votar`)
+}
 
 // 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable
 //    dependiendo de la edad 
 
+    edad >= 18 ? console.log("adulto") : console.log("menor")
+
 // 6. Muestra en que estación del año nos encontramos dependiendo del valor de una variable "mes"
+
+ if (mes === "diciembre" || mes === "enero" || mes === "febrero") {
+    console.log("Invierno")
+ } else if (mes === "marzo" || mes === "abril" || mes === "mayo") {
+    console.log("Primavera")
+ } else if (mes === "junio" || mes === "julio" || mes === "agosto") {
+    console.log("Verano")
+ } else if (mes === "septiembre" || mes === "octubre" || mes === "noviembre") {
+    console.log("Otoño")
+ }
 
 // 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior
 
-// switch
+    if (mes === "enero" || mes === "marzo" || mes === "mayo" || mes === "julio" || mes === "agosto" || mes === "octubre" || mes === "diciembre") {
+        console.log("Este mes tiene 31 días")
+    } else if (mes === "abril" || mes === "junio" || mes === "septiembre" || mes === "noviembre") {
+        console.log("Este mes tiene 30 días")
+    } else if (mes === "febrero") {
+        console.log("Este mes tiene 28 días")
+    }
 
 // 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
 
+switch (idioma) {
+    case "español":
+        console.log("Hola")
+        break
+    case "ingles":
+        console.log("Hello")
+        break
+    case "frances":
+        console.log("Bonjour")
+        break
+}
+
 // 9. Usa un switch para hacer de nuevo el ejercicio 6
 
+switch (mes) {
+    case "diciembre" || "enero" || "febrero":
+        console.log("Invierno")
+        break
+    case "marzo" || "abril" || "mayo":
+        console.log("Primavera")
+        break
+    case "junio" || "julio" || "agosto":
+        console.log("Verano")
+        break
+    case "septiembre" || "octubre" || "noviembre":
+        console.log("Otoño")
+        break
+}
+
 // 10. Usa un switch para hacer de nuevo el ejercicio 7
+
+switch (mes) {
+    case "enero" || "marzo" || "mayo" || "julio" || "agosto" || "octubre" || "diciembre":
+        console.log("Este mes tiene 31 días")
+        break
+    case "abril" || "junio" || "septiembre" || "noviembre":
+        console.log("Este mes tiene 30 días")
+        break
+    case "febrero":
+        console.log("Este mes tiene 28 días")
+        break
+}
